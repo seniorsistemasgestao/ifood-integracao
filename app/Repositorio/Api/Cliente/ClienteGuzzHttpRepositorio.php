134b822a51d28496b3bfe0372ec333623bd7df31
Senior Sistemas <seniorsistemasgestao@gmail.com>
2022-11-22 15:55:24 -0300

    up

--- app/Repositorio/Api/Cliente/ClienteGuzzHttpRepositorio.php
+++ app/Repositorio/Api/Cliente/ClienteGuzzHttpRepositorio.php
@@ -14,32 +14,48 @@ class ClienteGuzzHttpRepositorio
     public function __construct()
     {
         $this->urlBase = "https://merchant-api.ifood.com.br/";
-        
     }
 
     public function getCliente()
     {
         return new \GuzzleHttp\Client();
     }
-
+    // gerar o token de autenticação 
     public function getUrlToken()
     {
         return $this->urlBase . "authentication/v1.0/oauth/token";
     }
 
+    //sesssão de produtos lista-produtos
     public function getUrlProdutos($mercadoId)
     {
-        ///merchants/{merchantId}/products
-        return $this->urlBase."catalog/v1.0/merchants/{$mercadoId}/products";
+
+        return $this->urlBase . "catalog/v1.0/merchants/{$mercadoId}/products";
     }
 
+    // criar produto
+    public function getUrlPostProduto($mercadoId)
+    {
+        return $this->urlBase . "catalog/v1.0/merchants/{$mercadoId}/products";
+    }
+
+    //sessão de catalogo - lista de catalogos
     public function getUrlGetCalalogo($mercadoId)
     {
-        return $this->urlBase."catalog/v1.0/merchants/{$mercadoId}/catalogs";
+        return $this->urlBase . "catalog/v1.0/merchants/{$mercadoId}/catalogs";
     }
 
-    public function getUrlCategoriaID($mercadoId)
+    //sessão de categoria - lista de categorias
+    public function getUrlCategoria($mercadoId, $catalogoId)
     {
-        return $this->urlBase."";
+        return $this->urlBase . "catalog/v1.0/merchants/{$mercadoId}/catalogs/{$catalogoId}/categories";
     }
+
+    //sessão de item  - criar item
+    public function getUrlPostItems($mercadoId, $categoriaId, $produtoId)
+    {
+        return $this->urlBase . "catalog/v1.0/merchants/{$mercadoId}/categories/{$categoriaId}/products/{$produtoId}";
+    }
+
+
 }
