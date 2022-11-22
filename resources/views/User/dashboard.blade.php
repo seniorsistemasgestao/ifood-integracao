361beada63dda78017351e932aa326237b6a2398
Senior Sistemas <seniorsistemasgestao@gmail.com>
2022-11-22 14:32:13 -0300

    up

--- resources/views/User/dashboard.blade.php
+++ resources/views/User/dashboard.blade.php
@@ -6,39 +6,44 @@
 
     <h1 class="text-center">Parabens vc acabou de logar !</h1>
 
-@if (isset($produtos->elements)):
-    
+    @if (isset($produtos->elements)):
 
-    <div class="text-center justfy-conten-center">
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Descrição</th>
-                    <th scope="col">Codigo externo</th>
-                    <th scope="col"> Restrições alimentares</th>
-                    <th scope="col"> Serie</th>
-                    <th scope="col"> Quantidade</th>
-                    <th scope="col"> Unidade</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($produtos->elements as $produto)
+        <h1>Produtos cadastrados {{$produtos->count}}</h1> 
+        {{-- dd($produtos->elements) --}}
+        <div class="text-center container container-fluid justfy-conten-center">
+
+            <table class="table">
+                <thead>
                     <tr>
-                        <th scope="row">{{ $produto->id ?? "" }}</th>
-                       
-                        <td>{{$produto->name ?? ""}}</td>
-                        <td>{{$produto->description ?? ""}}</td>
-                        <td>{{$produto->externalCode ?? ""}}</td>
-                        
+                        <th scope="col">ID</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Descrição</th>
+                        <th scope="col">Codigo externo</th>
+                        <th scope="col"> Informações adicionais</th>
+                        <th scope="col"> Serie</th>
+                        <th scope="col"> Quantidade</th>
+                        <th scope="col"> Unidade</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($produtos->elements as $produto)
+                        <tr>
+                            <th scope="row">{{ $produto->id ?? 'Não informado' }}</th>
 
-    </div>
+                            <td>{{ $produto->name ?? 'Não informado' }}</td>
+                            <td>{{ $produto->description ?? 'Não informado' }}</td>
+                            <td>{{ $produto->externalCode ?? 'Não informado' }}</td>
+                            <td>{{ $produto->additionalInformation ?? 'Não informado' }}</td>
+                            <td>{{ $produto->ean ?? 'Não informado' }}</td>
+                            <td>{{ $produto->weight->quantity ?? 'Não informado' }}</td>
+                            <td>{{ $produto->weight->unit ?? 'Não informado' }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+        </div>
 
     @endif
 @endsection
